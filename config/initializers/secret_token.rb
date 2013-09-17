8358bbc68eca56cf06b7c667b23c499f4fc06300
Erica Kwan <erica@codeforamerica.org>
2013-09-17 16:03:17 -0700

    Initial rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EngineLight::Application.config.secret_key_base = 'f20cbb8987f74f5c5f2c0d91b1425d926891655d7477e60729e75bd1676d36553bd3e0c46e64bbfded2f7055df29feaa0bd7df380d88ae44bbe5f92d04a4a29b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EngineLight::Application.config.session_store :cookie_store, key: '_engine-light_session'
