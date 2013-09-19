6888114ae0a1f10ea77b283e13abd63a61054123
Erica Kwan <erica@codeforamerica.org>
2013-09-19 09:52:59 -0700

    Start of building out the web app dashboard page
    - Little bit of cleanup
    - Redirecting to 404 page when trying to access non-existent app

--- app/controllers/web_applications_controller.rb
+++ app/controllers/web_applications_controller.rb
@@ -1,2 +1,5 @@
 class WebApplicationsController < ApplicationController
+  def show
+    @web_app = WebApplication.find_by_name(params[:id]) || raise_not_found
+  end
 end
--- app/models/web_application.rb
+++ app/models/web_application.rb
@@ -1,6 +1,12 @@
 class WebApplication < ActiveRecord::Base
   include Requester
 
+  validates_uniqueness_of :name
+
+  def to_param
+    name
+  end
+
   def get_status
     get(status_url)
   end
