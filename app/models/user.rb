7983eaa86eb829806d61deead1916866c0edb53c
Erica Kwan <erica@codeforamerica.org>
2013-09-27 17:07:44 -0700

    Show the important part of the email to make the UI more readable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
 
   validates_presence_of :email
   has_and_belongs_to_many :web_applications, autosave: true
+
+  def local_email_part
+    email.split(/@(cfa|codeforamerica)\.org$/).first
+  end
 end
