9dc8d9f427e9467240b6467749972f2f49c88451
Erica Kwan <erica@codeforamerica.org>
2013-09-30 16:45:51 -0700

    Add link to app and show other app managers

--- app/helpers/web_applications_helper.rb
+++ app/helpers/web_applications_helper.rb
@@ -1,2 +1,7 @@
 module WebApplicationsHelper
+  def additional_managers(web_app, current_user)
+    managers = web_app.users - [current_user]
+    managers_string = managers.map(&:local_email_part).join(",")
+    managers_string.empty? ? "none" : managers_string
+  end
 end
