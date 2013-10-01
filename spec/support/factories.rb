97b58a9fb0830f1c1db39d34df82586ac28a40c3
Erica Kwan <erica@codeforamerica.org>
2013-10-01 09:36:48 -0700

    Fix interpolation error

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:email) {|n| "test_user{n}@cfa.org" }
+    sequence(:email) {|n| "test_user#{n}@cfa.org" }
   end
 
   factory :user_with_web_application, parent: :user do
