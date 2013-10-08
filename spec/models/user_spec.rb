434f80bfd877e311dd40db9e803f0c048da1539d
Erica Kwan <erica@codeforamerica.org>
2013-10-08 16:57:02 -0700

    Display entire email, colour rows according to status

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,4 @@
 require 'spec_helper'
 
 describe User do
-  describe "#local_email_part" do
-    it "returns the local part of the email" do
-      user = FactoryGirl.create(:user, email: "erica.p.kwan@codeforamerica.org")
-      user.local_email_part.should == "erica.p.kwan"
-    end
-  end
 end
