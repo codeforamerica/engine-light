58b30d2947ff80e537ec53788bcf05b597945c77
Erica Kwan <erica@codeforamerica.org>
2013-10-08 17:10:39 -0700

    Make the table deal with emails better

--- app/helpers/web_applications_helper.rb
+++ app/helpers/web_applications_helper.rb
@@ -1,6 +1,6 @@
 module WebApplicationsHelper
   def owners(web_app, current_user)
-    owners_string = web_app.users.map(&:email).join(",")
+    owners_string = web_app.users.map(&:email).join(", ")
     owners_string.empty? ? "none" : owners_string
   end
 end
