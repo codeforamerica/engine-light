21b744e92845f86d9c0be488fe24d8554cccf207
Erica Kwan <erica@codeforamerica.org>
2013-10-21 14:29:48 -0700

    Apps have a lot of height in prod, show less per page

--- app/models/web_application.rb
+++ app/models/web_application.rb
@@ -12,6 +12,8 @@ class WebApplication < ActiveRecord::Base
   friendly_id :name, use: :slugged
   attr_accessor :status_checked_at, :resources, :dependencies
 
+  self.per_page = 10
+
   def get_current_status
     begin
       response = get(status_url)
