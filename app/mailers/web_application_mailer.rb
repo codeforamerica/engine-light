25e8d927f9288bbc60dcae676da3b3d7c113c269
Erica Kwan <erica@codeforamerica.org>
2013-10-21 18:59:20 -0700

    Link to app detail page so users can see what's up easier

--- app/mailers/web_application_mailer.rb
+++ app/mailers/web_application_mailer.rb
@@ -3,14 +3,13 @@ class WebApplicationMailer < ActionMailer::Base
 
   def outage_notification(web_application)
     user_emails = web_application.users.map(&:email)
-    @web_application_name = web_application.name
-    @web_application_current_status = web_application.current_status
+    @web_application = web_application
     mail(to: user_emails, subject: "#{web_application.name} is down")
   end
 
   def recovery_notification(web_application)
     user_emails = web_application.users.map(&:email)
-    @web_application_name = web_application.name
+    @web_application = web_application
     mail(to: user_emails, subject: "#{web_application.name} has recovered")
   end
 end
