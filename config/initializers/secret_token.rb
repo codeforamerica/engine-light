048f2b0e76435ef934fe2afa4687f887801b7ce9
Erica Kwan <erica@codeforamerica.org>
2013-12-19 09:16:42 -0800

    Remove key cause it could be exploited to do bad things to the app

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-EngineLight::Application.config.secret_key_base = 'f20cbb8987f74f5c5f2c0d91b1425d926891655d7477e60729e75bd1676d36553bd3e0c46e64bbfded2f7055df29feaa0bd7df380d88ae44bbe5f92d04a4a29b'
+EngineLight::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
