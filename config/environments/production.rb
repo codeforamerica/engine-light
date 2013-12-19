6d35bad56a748ca002891f8a77e83120abef0406
Erica Kwan <erica@codeforamerica.org>
2013-12-19 11:30:32 -0800

    Remove prod config so it defaults to application config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,10 +24,6 @@ EngineLight::Application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
